70db7011dc2d854b2d8a0d8e71150a0431aec8a6
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-12-15 11:15:35 +0900

    initial set

--- mdn-tutorial/01_basic_usage/script.js
+++ mdn-tutorial/01_basic_usage/script.js
@@ -1,3 +1,13 @@
 'use strict';
 {
+  function draw() {
+    const canvas = document.getElementById('tutorial');
+
+    if (canvas.getContext) {
+      const canvasContext = canvas.getContext('2d');
+    } else {
+      console.log('This browser dose not support Canvas API.');
+    }
+  }
+  window.addEventListener('load', draw);
 }
