45e9982b73785fa67ec8426acaf7958d69db357f
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-12-18 17:33:48 +0900

    #9 simple draw (#13)
    * マウスをクリックして動いている間の挙動を確認
    
    * この状態だと絵を描く位置がずれていて、スピードが早いと線が途切れる
    
    * canvasの線についてのメモ
    
    * 作れた、でも線というか、図形なので途切れる
    
    * 仕組みわかってないけどできた！
    
    * beginPath、strokeで解決
    
    * シンプルに書けるアプリの完成かな
    
    * メモ追加

---
+++ 01_simple-draw/script.js
@@ -0,0 +1,78 @@
+'use strict';
+{
+  /* 
+    [仕様]
+    - ペイントの色は黒でOK
+    - とにかくベーシックに線がかければOK
+  */
+
+  // カンバスの背景の指定方法は？
+  // カンバスを枠でくくるには
+  const canvas = document.getElementById('canvas');
+
+  if (canvas.getContext) {
+    const ctx = canvas.getContext('2d');
+
+    // canvasのサイズ、位置情報を取得
+    const canvasElementInfo = canvas.getBoundingClientRect();
+    console.log(canvasElementInfo);
+
+    // x、yを定義、のちにマウスの位置との計算に使用
+    const canvasX = canvasElementInfo.x;
+    const canvasY = canvasElementInfo.y;
+    console.log(canvasX, canvasY);
+
+    let isDrawing = false;
+
+    // TODO: 画面外にでて、勝手にペンがおわると案外うっとおしい。
+    // 画面外に出てもいいけど、外部でマウスをはなしたらきちんとisDrawingが取れるようにしたい
+    // 次でOK
+
+    /* 
+      [PROBLEM] 絵をかけるようになったけど、マウスをはやく動かすと線が途切れる
+      - 解決: beginPath, lineTo()
+
+      [PROBLEM] マウスダウンして、クリックした時だけでも絵をかけるようにしたいけど、moveTo、lineTo使っても点ができない
+      - 解決: stroke()を書いて解決。描画してなかったからできなかった。
+    */
+
+    /* 
+      線のデザイン: https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Applying_styles_and_colors#line_styles
+    */
+
+    /* 
+    - mousedown: もちろんマウスをおろしたとき、クリックした時発動する
+    - mouseenter, mouseleave, mouseout -> 画面領域に入った時、出た時まわりの動き
+    - mousemove: 要素にはいって動いている間
+    */
+    canvas.addEventListener('mousedown', (e) => {
+      isDrawing = true;
+
+      ctx.beginPath();
+      ctx.strokeStyle = '#ff1493';
+      ctx.lineWidth = 8;
+      ctx.lineJoin = 'round';
+      ctx.lineCap = 'round';
+
+      ctx.lineTo(e.clientX - canvasX, e.clientY - canvasY);
+      ctx.stroke();
+
+      canvas.addEventListener('mousemove', (e) => {
+        if (isDrawing) {
+          ctx.lineTo(e.clientX - canvasX, e.clientY - canvasY);
+          ctx.stroke();
+        }
+      });
+    });
+
+    canvas.addEventListener('mouseup', (e) => {
+      isDrawing = false;
+    });
+
+    canvas.addEventListener('mouseout', (e) => {
+      isDrawing = false;
+    });
+  } else {
+    console.warn('There is no support.');
+  }
+}
